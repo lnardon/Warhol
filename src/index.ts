bd924501c38018b2ab449bb1e316064c37edc03f
lnardon <lucaslnardon@gmail.com>
2023-05-08 01:42:21 -0300

    ✨ Add all basic endpoints

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import fs from "fs";
 dotenv.config();
 const app: Express = express();
 const PORT = process.env.PORT;
+app.use(express.static("../frontend/build"));
 
 const storage = multer.diskStorage({
   destination: function (req: any, file: any, cb: any) {
@@ -37,73 +38,161 @@ const upload = multer({
 }).array("images");
 
 app.get("/", async (req: Request, res: Response) => {
-  res.json({ howdy: "there partner" });
+  console.log(path.join(__dirname, "..", "/frontend", "/build", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "/frontend", "/build", "index.html"));
 });
 
-app.post(
-  "/optimization/losslessWebp",
-  upload,
-  async (req: Request, res: Response) => {
-    const files = req.files as Express.Multer.File[];
-    let convertedFiles: string[] = [];
+app.post("/losslessWebp", upload, async (req: Request, res: Response) => {
+  const files = req.files as Express.Multer.File[];
+  let convertedFiles: string[] = [];
 
-    for (let file of files) {
-      const filePath = path.join(__dirname, "/uploads/" + file.filename);
-      const outputFilePath = path.resolve(
-        __dirname,
-        "converted",
-        file.filename.split(".")[0] + ".webp"
-      );
+  for (let file of files) {
+    const filePath = path.join(__dirname, "/uploads/" + file.filename);
+    const outputFilePath = path.resolve(
+      __dirname,
+      "converted",
+      file.filename.split(".")[0] + ".webp"
+    );
 
-      await sharp(filePath).webp({ lossless: true }).toFile(outputFilePath);
+    await sharp(filePath).webp({ lossless: true }).toFile(outputFilePath);
 
-      convertedFiles.push(outputFilePath);
-    }
-
-    files.forEach((file, index) => {
-      fs.unlinkSync(file.path);
-    });
-    setTimeout(() => {
-      convertedFiles.forEach((file) => {
-        fs.unlinkSync(file);
-      });
-    }, 1000 * 3600); // 3600 seconds || 1 Hour
-    res.send(convertedFiles);
+    convertedFiles.push(outputFilePath);
   }
-);
 
-app.post(
-  "/manipulation/greyscale",
-  upload,
-  async (req: Request, res: Response) => {
-    const files = req.files as Express.Multer.File[];
-    let convertedFiles: string[] = [];
-
-    for (let file of files) {
-      const filePath = path.join(__dirname, "/uploads/" + file.filename);
-      const outputFilePath = path.resolve(
-        __dirname,
-        "converted",
-        file.filename
-      );
-
-      await sharp(filePath).grayscale().toFile(outputFilePath);
-
-      convertedFiles.push(outputFilePath);
-    }
-
-    files.forEach((file, index) => {
-      fs.unlinkSync(file.path);
+  files.forEach((file, index) => {
+    fs.unlinkSync(file.path);
+  });
+  setTimeout(() => {
+    convertedFiles.forEach((file) => {
+      fs.unlinkSync(file);
     });
-    setTimeout(() => {
-      convertedFiles.forEach((file) => {
-        fs.unlinkSync(file);
-      });
-    }, 1000); // 3600 seconds || 1 Hour
-    res.send(convertedFiles);
-  }
-);
+  }, 1000 * 3600); // 3600 seconds || 1 Hour
+  res.send(convertedFiles);
+});
 
-app.listen(PORT, () => {
+app.post("/greyscale", upload, async (req: Request, res: Response) => {
+  const files = req.files as Express.Multer.File[];
+  let convertedFiles: string[] = [];
+
+  for (let file of files) {
+    const filePath = path.join(__dirname, "/uploads/" + file.filename);
+    const outputFilePath = path.resolve(__dirname, "converted", file.filename);
+
+    await sharp(filePath).grayscale().toFile(outputFilePath);
+
+    convertedFiles.push(outputFilePath);
+  }
+
+  files.forEach((file, index) => {
+    fs.unlinkSync(file.path);
+  });
+  setTimeout(() => {
+    convertedFiles.forEach((file) => {
+      fs.unlinkSync(file);
+    });
+  }, 1000); // 3600 seconds || 1 Hour
+  res.send(convertedFiles);
+});
+
+app.post("/blur", upload, async (req: Request, res: Response) => {
+  const files = req.files as Express.Multer.File[];
+  const blurAmount = JSON.parse(req.body);
+  let convertedFiles: string[] = [];
+
+  for (let file of files) {
+    const filePath = path.join(__dirname, "/uploads/" + file.filename);
+    const outputFilePath = path.resolve(__dirname, "converted", file.filename);
+
+    await sharp(filePath).blur(blurAmount).toFile(outputFilePath);
+
+    convertedFiles.push(outputFilePath);
+  }
+
+  files.forEach((file, index) => {
+    fs.unlinkSync(file.path);
+  });
+  setTimeout(() => {
+    convertedFiles.forEach((file) => {
+      fs.unlinkSync(file);
+    });
+  }, 1000); // 3600 seconds || 1 Hour
+  res.send(convertedFiles);
+});
+
+app.post("/flip", upload, async (req: Request, res: Response) => {
+  const files = req.files as Express.Multer.File[];
+  let convertedFiles: string[] = [];
+
+  for (let file of files) {
+    const filePath = path.join(__dirname, "/uploads/" + file.filename);
+    const outputFilePath = path.resolve(__dirname, "converted", file.filename);
+
+    await sharp(filePath).flip().toFile(outputFilePath);
+
+    convertedFiles.push(outputFilePath);
+  }
+
+  files.forEach((file, index) => {
+    fs.unlinkSync(file.path);
+  });
+  setTimeout(() => {
+    convertedFiles.forEach((file) => {
+      fs.unlinkSync(file);
+    });
+  }, 1000); // 3600 seconds || 1 Hour
+  res.send(convertedFiles);
+});
+
+app.post("/rotate", upload, async (req: Request, res: Response) => {
+  const files = req.files as Express.Multer.File[];
+  const degrees = JSON.parse(req.body);
+  let convertedFiles: string[] = [];
+
+  for (let file of files) {
+    const filePath = path.join(__dirname, "/uploads/" + file.filename);
+    const outputFilePath = path.resolve(__dirname, "converted", file.filename);
+
+    await sharp(filePath).rotate(degrees).toFile(outputFilePath);
+
+    convertedFiles.push(outputFilePath);
+  }
+
+  files.forEach((file, index) => {
+    fs.unlinkSync(file.path);
+  });
+  setTimeout(() => {
+    convertedFiles.forEach((file) => {
+      fs.unlinkSync(file);
+    });
+  }, 1000); // 3600 seconds || 1 Hour
+  res.send(convertedFiles);
+});
+
+app.post("/resize", upload, async (req: Request, res: Response) => {
+  const files = req.files as Express.Multer.File[];
+  const { width, height } = JSON.parse(req.body);
+  let convertedFiles: string[] = [];
+
+  for (let file of files) {
+    const filePath = path.join(__dirname, "/uploads/" + file.filename);
+    const outputFilePath = path.resolve(__dirname, "converted", file.filename);
+
+    await sharp(filePath).resize({ width, height }).toFile(outputFilePath);
+
+    convertedFiles.push(outputFilePath);
+  }
+
+  files.forEach((file, index) => {
+    fs.unlinkSync(file.path);
+  });
+  setTimeout(() => {
+    convertedFiles.forEach((file) => {
+      fs.unlinkSync(file);
+    });
+  }, 1000); // 3600 seconds || 1 Hour
+  res.send(convertedFiles);
+});
+
+app.listen(PORT || 3333, () => {
   console.log(`⚡️[server]: Server is running at port: ${PORT}`);
 });
