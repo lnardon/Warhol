90abf1fbc8178363c37f9900b98529f7c6056266
Lucas Nardon <43593024+lnardon@users.noreply.github.com>
2023-10-31 23:22:40 +0000

    Add basic style for frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,41 @@
+import {useState} from "react"
 import "./App.css";
 
 function App() {
+  const [format,setFormat] = useState("png");
+
+  function handleFileExplorerOpen (){
+    document.getElementById("image_picker")?.click()
+  }
+
+  function handleFormatSelect(e: any){
+    setFormat(e.target.value)
+  }
+
+  function sendFilesToConversion(){
+    fetch("/upload", {
+      method: "POST",
+      body: JSON.stringify({
+        format
+      })
+    }).then(e => e.json()).then(e => console.log(e))
+  }
+
   return (
     <div className="App">
       <header className="header">
         <h1 className="title">WARHOL</h1>
       </header>
       <div className="content">
-        <button className="uploadBtn">Add images</button>
+        <select name="image_format" onChange={handleFormatSelect} value={format}>
+          <option value="webp">WEBP</option>
+          <option value="png">PNG</option>
+          <option value="jpg">JPEG</option>
+          <option value="gif">GIF</option>
+        </select>
+        <input type="file" name="Image" id="image_picker" accept="image/png, image/jpeg. image/webp"/>
+        <button className="uploadBtn" onClick={handleFileExplorerOpen}>Add images</button>
+        <button className="sendBtn" onClick={sendFilesToConversion}>Convert images</button>
       </div>
     </div>
   );
